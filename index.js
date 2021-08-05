cc4fc0ee7fde29917eb4e96d25ca725644f7d840
shl422 <h.k.domingo@outlook.com>
2021-08-05 09:27:47 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'the bob';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+}
+
+function attemptTwoFavoriteCustomers() {
+
+    throw SyntaxError();
+}
